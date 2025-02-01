cddb96b6114fcf3a622c1efa004b4c416c549f58
Laturas <laturaas@gmail.com>
2025-02-01 17:30:29 -0500

    backend is basically done

--- src/requester.py
+++ src/requester.py
@@ -17,7 +17,7 @@ def read_item(video_id: str) -> tuple[int, int]:
 
         if response.status_code == 200:
             posts = response.json()
-            print(posts)
+            #print(posts)
             content_type = int(posts["items"][0]["snippet"]["categoryId"])
             dur_string = posts["items"][0]["contentDetails"]["duration"]
 
@@ -27,7 +27,12 @@ def read_item(video_id: str) -> tuple[int, int]:
             total_time = int(str(minute_split[0])[0:-1]) * 60 + int(str(second_split[0])[0:-1])
 
             print(f"{total_time} seconds")
-            return (content_type, total_time)
+            title: str = posts["items"][0]["snippet"]["title"]
+            description: str = posts["items"][0]["snippet"]["description"]
+            title_and_desc = f"Title: {title}\nDescription: {description}"
+            ai_response = dear_ai_is_this_brainrot(gemini_api_key, title_and_desc)
+
+            return (ai_response, total_time)
         else:
             print('Error:', response.status_code)
             return (0,0)
@@ -35,17 +40,18 @@ def read_item(video_id: str) -> tuple[int, int]:
         print('Error:', e)
         return (0,0)
 
-def dear_ai_is_this_brainrot(gemini_api_key: str, video_transcript: str) -> int:
+def dear_ai_is_this_brainrot(api_key: str, video_transcript: str) -> int:
     # Set your API key
-    genai.configure(api_key = gemini_api_key)
+    genai.configure(api_key = api_key)
 
     # Choose a model
     model = genai.GenerativeModel("gemini-pro")
 
     # Generate a response
-    response = model.generate_content("You may only respond with \"entertainment\" or \"informative\". Is the following transcript entertainment or informative:\n" + video_transcript)
-    if response == "informative": return 1
-    if response == "entertainment": return 2
+    response = model.generate_content("You may only respond with \"entertainment\" or \"informative\". This is the title and description of a youtube video. Do you think this video is entertainment or informative?\n" + video_transcript)
+    print(response.text)
+    if response.text == "informative": return 1
+    if response.text == "entertainment": return 2
     return 0
 
-print(read_item("B8VR5mQcgjI"))
+print(read_item("ZvC2jtmVAMs"))
