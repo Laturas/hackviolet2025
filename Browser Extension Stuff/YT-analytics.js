aeceff861dc2eaf39ba84e4d26ca310cb1e2ea79
Birch <ethantwombly3@gmail.com>
2025-02-01 13:55:19 -0500

    Setup Extension Base
    Added new folder for extension code
    Got basic script/boilerplate/icon stuffs

---
+++ Browser Extension Stuff/YT-analytics.js
@@ -0,0 +1,10 @@
+
+
+// Runs every time a youtube page is loaded
+
+// Put all tracking code here
+
+
+// just to see if it is working
+document.body.style.border = "5px solid red";
+console.log("===========================\ninfiltrated the system\n===========================");
